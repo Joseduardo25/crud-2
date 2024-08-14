00e77b5412f7d0091a3b6a8b9d284524826b6c0d
Eduardo Calachua <jcalachua@dtransforma.com>
2024-08-14 01:06:37 -0500

    add: API services post and get

--- src/app/api/servicios/route.js
+++ src/app/api/servicios/route.js
@@ -4,8 +4,20 @@ import clientPromise from "@/libs/mongoDB";
 
 
 export const GET = async () => {
-  const client = clientPromise
+  const client = await clientPromise
+  const db = client.db("marketplace-sell")
+  const items = await db.collection("items").find({}).toArray()
+  return NextResponse.json(items, {status: 200})
+}
+
+export const POST = async(req) =>{
+  const client = await clientPromise
+  const db = client.db("marketplace-sell")
+  const {data} = await req.json()
+
+  const items = await db.collection("items").find({}).toArray()
   return NextResponse.json({
-    message: "success"
+    message: "successful shipment",
+    items
   })
 }
